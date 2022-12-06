ed709003d9a805b604c6c642cb00849be7a19a53
Pär Parkred <par.parkred@getaccept.com>
2022-12-06 11:50:29 +0100

    day 6

---
+++ 6/index.ts
@@ -0,0 +1,22 @@
+import { readFileSync } from "fs";
+const file: string = readFileSync("input.txt", "utf8");
+const fileArray = file.split("");
+
+export const getParsedValue = (split: number) => {
+  for (let i = 0; i < fileArray.length; i++) {
+    const arrayToCheck = fileArray.slice(i, i + split);
+    const itemExists = arrayToCheck.some((element, index) => {
+      return (
+        arrayToCheck.includes(element) &&
+        arrayToCheck.indexOf(element) !== index
+      );
+    });
+    if (!itemExists) {
+      console.log(i + split);
+      break;
+    }
+  }
+};
+
+getParsedValue(4);
+getParsedValue(14);
