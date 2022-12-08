faa86448baba73307177c50bb8eedcbbf3024e62
Pär Parkred <par.parkred@getaccept.com>
2022-12-08 18:26:54 +0100

    day 7 part one :D

--- 7/index.ts
+++ 7/index.ts
@@ -1,26 +1,47 @@
-import { readFileSync } from "fs";
-const file: string = readFileSync("input.txt", "utf8");
+import * as fs from "fs";
+import { execSync } from "child_process";
+var path = require("path");
+const file: string = fs.readFileSync("input.txt", "utf8");
 const fileArray = file.split("\n");
+const filePath: string[] = ["teesting/"];
 
-interface directory {
-  name: string;
-  fileSize: number;
-  parentFolder?: string;
-  subDirectories?: directory[];
-}
-const set: Set<directory> = new Set();
-const filePath: string[] = [];
-//TODO
-
-fileArray.map((element, index) => {
-  const [elementOne, elementTwo, elementThree] = element.split(" ");
-  if (elementTwo === "cd") {
-    if (elementThree === "..") {
-      filePath.pop();
-      console.log(filePath);
-    } else {
-      filePath.push(elementThree);
-      console.log(filePath);
+export const setUpFileStructure = () => {
+  fileArray.map((element, index) => {
+    const [elementOne, elementTwo, elementThree] = element.split(" ");
+    if (elementTwo === "cd") {
+      if (elementThree === "..") {
+        filePath.pop();
+        console.log(filePath);
+      } else {
+        if (elementThree !== "/") {
+          filePath.push(`${elementThree}/`);
+        }
+      }
+    } else if (elementOne === "dir") {
+      execSync(`cd ${filePath.join("")} && mkdir ${elementTwo}`);
+    } else if (parseInt(elementOne)) {
+      execSync(`cd ${filePath.join("")} && touch ${elementOne}-${elementTwo}`);
     }
+  });
+};
+
+let resultOne = 0;
+
+function traverseDir(dir): number {
+  let result = 0;
+  fs.readdirSync(dir).forEach((file) => {
+    let fullPath = path.join(dir, file);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      result += traverseDir(fullPath);
+    } else {
+      const fileName = fullPath.split("/").pop().split("-")[0];
+      result += +fileName;
+    }
+  });
+  if (result < 100000) {
+    resultOne += result;
   }
-});
+  return result;
+}
+traverseDir("teesting");
+console.log(resultOne);
